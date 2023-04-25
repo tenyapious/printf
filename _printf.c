e0588e700e3b0707142e344e85e7248b8eb2b606
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-04-25 22:49:36 +0000

    Modification to _printf.c file

--- _printf.c
+++ _printf.c
@@ -4,105 +4,139 @@
 #include <stdarg.h>
 
 /**
- * _printf - outputs a string after it has been properly formatted
+ * get_size - calculate the length of output string
+ * @format: input string
+ * @ap: argument list
  *
- * @format: inputs that determine how printf handles and returns the string
- *
- * Return: returns and prints a formatted string
+ * Return: size of output string
+ */
+int get_size(const char *format, va_list ap)
+{
+	int i, k;
+
+	char __attribute__ ((unused)) c;
+	char *s;
+	int args_len = 0;
+	int format_len = 0;
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == 'c')
+			{
+				c = (char)va_arg(ap, int);
+				args_len++;
+				format_len -= 2;
+			}
+			else if (format[i + 1] == 's')
+			{
+				s = va_arg(ap, char *);
+				for (k = 0; s[k] != '\0';)
+				{
+					k++;
+					args_len++;
+				}
+				format_len -= 2;
+			}
+		}
+		format_len++;
+	}
+	return (args_len + format_len);
+}
+
+
+/**
+ * output_str - combine input string and arguments
+ * @str: allocated memory for output string
+ * @format: input string
+ * @ap: argument list
  *
  */
 
+void output_str(char *str, const char *format, va_list ap)
+{
+	int i, j, k;
+	char c;
+	char *s;
+
+	for (i = 0, j = 0; format[i] != '\0';)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == 'c')
+			{
+				c = (char)va_arg(ap, int);
+				str[j] = c;
+				j++;
+				i++;
+			}
+			else if (format[i + 1] == 's')
+			{
+				s = va_arg(ap, char *);
+				for (k = 0; s[k] != '\0';)
+				{
+					str[j] = s[k];
+					j++;
+					k++;
+				}
+				i++;
+			}
+			else if (format[i + 1] == '%')
+			{
+				str[j] = '%';
+				j++;
+				i++;
+			}
+			else
+			{
+				str[j] = format[i];
+				j++;
+			}
+		}
+		else
+		{
+			str[j] = format[i];
+			j++;
+		}
+		i++;
+	}
+	str[j] = '\0';
+}
+
+/**
+ * _printf - print formatted string
+ * @format: input string
+ *
+ * Return: length of formatted string
+ */
 int _printf(const char *format, ...)
 {
-    int i, j, k;
-    char *str;
-    char c;
-    char *s;
-    va_list ap;
-    int args_len = 0;
-    int format_len = 0;
-    int num_output_char;
+	char *str;
+	va_list ap;
+	int num_output_char;
 
-    va_start(ap, format);
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            if (format[i + 1] == 'c')
-            {
-                c = (char)va_arg(ap, int);
-                args_len++;
-                format_len -= 2;
-            }
-            else if (format[i + 1] == 's')
-            {
-                s = va_arg(ap, char *);
-                for (k = 0; s[k] != '\0';)
-                {
-                    k++;
-                    args_len++;
-                }
-                format_len -= 2;
-            }
-        }
-        format_len++;
-    }
-    va_end(ap);
+	va_start(ap, format);
+	num_output_char = get_size(format, ap);
 
-    num_output_char = format_len + args_len;
+	/**
+	va_end(ap);
 
-    str = malloc(sizeof(*str) * (num_output_char + 1));
+	str = malloc(sizeof(*str) * (num_output_char + 1));
+	if (!str)
+	{
+		return (0);
+	}
 
-    va_start(ap, format);
-    for (i = 0, j = 0; format[i] != '\0';)
-    {
-        if (format[i] == '%')
-        {
-            if (format[i + 1] == 'c')
-            {
-                c = (char)va_arg(ap, int);
-                str[j] = c;
-                j++;
-                i++;
-            }
-            else if (format[i + 1] == 's')
-            {
-                s = va_arg(ap, char *);
-                for (k = 0; s[k] != '\0';)
-                {
-                    str[j] = s[k];
-                    j++;
-                    k++;
-                }
-                i++;
-            }
-            else if (format[i + 1] == '%')
-            {
-                str[j] = '%';
-                j++;
-                i++;
-            }
-            else
-            {
-                str[j] = format[i];
-                j++;
-            }
-        }
-        else
-        {
-            str[j] = format[i];
-            j++;
-        }
-        i++;
-    }
+	va_start(ap, format);
 
-    va_end(ap);
+	*/
+	output_str(str, format, ap);
+	va_end(ap);
 
-    str[j] = '\0';
+	write(STDOUT_FILENO, str, sizeof(char) * num_output_char);
 
-    write(STDOUT_FILENO, str, sizeof(char) * num_output_char);
+	/* free(str); */
 
-    free(str);
-
-    return (num_output_char);
+	return (num_output_char);
 }
