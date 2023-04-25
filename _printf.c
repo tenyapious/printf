efcf43d4f888029d591f0d39cce54bbb94ab7407
tenyapious <tenyapious@gmail.com>
2023-04-25 12:01:58 -0700

    use explicit allocated mem

--- _printf.c
+++ _printf.c
@@ -1,15 +1,48 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
-#include <string.h>
 
 int _printf(const char *format, ...)
 {
     int i, j, k;
-    char str[100];
-    va_list ap;
+    char *str;
     char c;
     char *s;
+    va_list ap;
+    int args_len = 0;
+    int format_len = 0;
+    int num_output_char;
+
+    va_start(ap, format);
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] == '%')
+        {
+            if (format[i + 1] == 'c')
+            {
+                c = (char)va_arg(ap, int);
+                args_len++;
+                format_len -= 2;
+            }
+            else if (format[i + 1] == 's')
+            {
+                s = va_arg(ap, char *);
+                for (k = 0; s[k] != '\0';)
+                {
+                    k++;
+                    args_len++;
+                }
+                format_len -= 2;
+            }
+        }
+        format_len++;
+    }
+    va_end(ap);
+
+    num_output_char = format_len + args_len;
+
+    str = malloc(sizeof(*str) * (num_output_char + 1));
 
     va_start(ap, format);
     for (i = 0, j = 0; format[i] != '\0';)
@@ -58,7 +91,9 @@ int _printf(const char *format, ...)
 
     str[j] = '\0';
 
-    write(STDOUT_FILENO, str, sizeof(char) * strlen(str));
+    write(STDOUT_FILENO, str, sizeof(char) * num_output_char);
 
-    return (0);
+    free(str);
+
+    return (num_output_char);
 }
--- test/main.c
+++ test/main.c
@@ -9,6 +9,20 @@
  */
 int main(void)
 {
-	_printf("This is %c t%coo Stressful%%, %s!\n", 'A', 'w', "anyways");
+	int __attribute__ ((unused)) len;
+	int __attribute__ ((unused)) len2;
+	void *addr;
+	addr = (void *)0x7ffe637541f0;
+
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	_printf("Address:[%p]\n", addr);
+	printf("Address:[%p]\n", addr);
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	_printf("Unknown:[%r]\n");
+	printf("Unknown:[%r]\n");
 	return (0);
 }
