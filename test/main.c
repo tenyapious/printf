c40cc285061d53eac79cf9c097af9cea3b08ab37
tenyapious <tenyapious@gmail.com>
2023-04-25 19:24:43 -0700

    factorize code

--- test/main.c
+++ test/main.c
@@ -9,11 +9,37 @@
  */
 int main(void)
 {
-	int __attribute__ ((unused)) len;
-	int __attribute__ ((unused)) len2;
+	int len;
+	int len2;
+	unsigned int ui;
 	void *addr;
-	addr = (void *)0x7ffe637541f0;
 
+	_printf("Hello wor%%\n");
+	printf("Hello wor%%\n");
+	_printf("%Hello wor%%\n");
+	printf("%Hello wor%%\n");
+	_printf("%%Hello wor%%\n");
+	printf("%%Hello wor%%\n");
+	_printf("%cHello wor%%\n");
+	printf("%cHello wor%%\n");
+	_printf("%sHello wor%%\n","string");
+	printf("%sHello wor%%\n","string");
+	_printf("%cHello wor%%\n", 'x', 'z');
+	printf("%cHello wor%%\n", 'x', 'z');
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	ui = (unsigned int)INT_MAX + 1024;
+	addr = (void *)0x7ffe637541f0;
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	_printf("Unsigned:[%u]\n", ui);
+	printf("Unsigned:[%u]\n", ui);
+	_printf("Unsigned octal:[%o]\n", ui);
+	printf("Unsigned octal:[%o]\n", ui);
+	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
 	_printf("Character:[%c]\n", 'H');
 	printf("Character:[%c]\n", 'H');
 	_printf("String:[%s]\n", "I am a string !");
@@ -22,6 +48,8 @@ int main(void)
 	printf("Address:[%p]\n", addr);
 	len = _printf("Percent:[%%]\n");
 	len2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
 	_printf("Unknown:[%r]\n");
 	printf("Unknown:[%r]\n");
 	return (0);
--- test/main.h
+++ test/main.h
@@ -2,3 +2,8 @@
 #define PRINTF
 int _printf(const char *format, ...);
 #endif
+
+#ifndef PUTCHAR
+#define PUTCHAR
+int _putchar(char c);
+#endif
