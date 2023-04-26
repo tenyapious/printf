764e5d3ea05ed6a77db0d5a8289baf06e26128be
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-04-26 15:46:46 +0000

    implement custom putchar prototype to write out result

--- _printf.c
+++ _printf.c
@@ -1,142 +1,89 @@
+#include "main.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
-/**
- * get_size - calculate the length of output string
- * @format: input string
- * @ap: argument list
- *
- * Return: size of output string
- */
-int get_size(const char *format, va_list ap)
+
+
+void _putcharc(char c)
 {
-	int i, k;
-
-	char __attribute__ ((unused)) c;
-	char *s;
-	int args_len = 0;
-	int format_len = 0;
-
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == 'c')
-			{
-				c = (char)va_arg(ap, int);
-				args_len++;
-				format_len -= 2;
-			}
-			else if (format[i + 1] == 's')
-			{
-				s = va_arg(ap, char *);
-				for (k = 0; s[k] != '\0';)
-				{
-					k++;
-					args_len++;
-				}
-				format_len -= 2;
-			}
-		}
-		format_len++;
-	}
-	return (args_len + format_len);
+	write(1, &c, 1);
 }
 
-
-/**
- * output_str - combine input string and arguments
- * @str: allocated memory for output string
- * @format: input string
- * @ap: argument list
- *
- */
-
-void output_str(char *str, const char *format, va_list ap)
+void _putchar(va_list params)
 {
-	int i, j, k;
 	char c;
-	char *s;
 
-	for (i = 0, j = 0; format[i] != '\0';)
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == 'c')
-			{
-				c = (char)va_arg(ap, int);
-				str[j] = c;
-				j++;
-				i++;
-			}
-			else if (format[i + 1] == 's')
-			{
-				s = va_arg(ap, char *);
-				for (k = 0; s[k] != '\0';)
-				{
-					str[j] = s[k];
-					j++;
-					k++;
-				}
-				i++;
-			}
-			else if (format[i + 1] == '%')
-			{
-				str[j] = '%';
-				j++;
-				i++;
-			}
-			else
-			{
-				str[j] = format[i];
-				j++;
-			}
-		}
-		else
-		{
-			str[j] = format[i];
-			j++;
-		}
-		i++;
-	}
-	str[j] = '\0';
+	c = va_arg(params, int);
+	write(1, &c, 1);
 }
 
-/**
- * _printf - print formatted string
- * @format: input string
- *
- * Return: length of formatted string
- */
+/* handle strings */
+void printstr(va_list params)
+{
+	char *c;
+	c = va_arg(params, char *);
+	
+	/* loop and handle nullbyte */
+	while (*c != '\0')
+	{
+		_putcharc(*c++);
+	}
+}
+
+
+/* handle integers */
+void print_int();
+{
+}
+
+
+
+/* main _printf function */
 int _printf(const char *format, ...)
 {
-	char *str;
-	va_list ap;
-	int num_output_char;
+	va_list params;
+	int i = 0;
+	int j = 0;
+	int counter = 0;
 
-	va_start(ap, format);
-	num_output_char = get_size(format, ap);
+	/* pass flags as a multidimensional array to loop through va */
+	cs_t dir = [] = {
+		{'c', _putchar},
+		{'s', print_str},
+		{'d', print_int},
+		{'i', print_int}
+	};
 
-	/**
-	va_end(ap);
 
-	str = malloc(sizeof(*str) * (num_output_char + 1));
-	if (!str)
-	{
+	/* check for NULL */
+	if (format == NULL)
 		return (0);
+
+	/* loop through format */
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			j = 0;
+			while (j < 4)
+			{
+				if (format[i + 1] == dir[j].cs && format[i + 1] != '%')
+				{
+					dir[j].f(a);
+					i++;
+				}
+				j++;
+			}
+			i++;
+			_putcharc(format[1]);
+		}
+		else
+			_putcharc(format[1]);
+
+		counter++;
+		i++;
 	}
-
-	va_start(ap, format);
-
-	*/
-	output_str(str, format, ap);
-	va_end(ap);
-
-	write(STDOUT_FILENO, str, sizeof(char) * num_output_char);
-
-	/* free(str); */
-
-	return (num_output_char);
+	return (counter);
 }
--- main.h
+++ main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+typedef struct dir{
+	char cs;
+	void(*f)();
+} cs_t;
+
+void _putcharc(char c);
+void _putchar(va_list a);
+void print_str(va_list a);
 
 
 int _printf(const char *format, ...);
